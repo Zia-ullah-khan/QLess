3f46ba4815e9bbdcced1a361d86950b0cae7ecf9
Zia-ullah-khan <khansokan1234@gmail.com>
2026-01-17 14:08:13 -0500

    feat: Add Mongoose Product model schema and initial backend environment variables.

--- backend/model/product.js
+++ backend/model/product.js
@@ -46,10 +46,6 @@ const productSchema = new mongoose.Schema(
 
 // Indexes
 productSchema.index({ store_id: 1 });
-<<<<<<< HEAD
-=======
-
->>>>>>> fdf174b88fe90107580da7e9bb477c8c235a8e98
 productSchema.index({ sku: 1 });
 
 export default mongoose.model('Product', productSchema);
